a890289e5e4ebc9d9110ff97b3d6796995112e04
gilstrong <114637250+gilstrong@users.noreply.github.com>
2025-05-16 03:10:34 -0400

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -20,10 +20,11 @@ app.use(session({
 }));
 
 // --- Conexión a MongoDB ---
-mongoose.connect('mongodb://127.0.0.1:27017/rutaReciclador', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
+
 .then(() => console.log('✅ Conectado a MongoDB'))
 .catch(err => console.error('❌ Error conectando a MongoDB:', err));
 
--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Conectar a MongoDB
 const connectDB = async () => {
   try {
-    await mongoose.connect('tu_cadena_de_conexion_a_mongo', {
+    await mongoose.connect('MONGO_URI=mongodb+srv://groupfive:groupfive@cluster0.zbfzlql.mongodb.net/reciclador?retryWrites=true&w=majority', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
